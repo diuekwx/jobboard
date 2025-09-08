99ba5ca272cf232882c7d7bf0dd54a266ab02dae
Jerison <jerisonw@gmail.com>
2025-09-08 14:06:38 -0700

    dashvboard

---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,43 @@
+import KanbanBoard from "../components/Kanban";
+import { API_BASE_URL } from "../api/api";
+
+const Dashboard = () => {
+
+  const ping = async () => {
+    const response = await fetch(`${API_BASE_URL}/api/user/ping`, {
+      method: "GET",
+      credentials: "include"
+    });
+    const data = await response.json();
+    if (data){
+      console.log(data);
+    }
+  };
+
+  const me = async () => {
+    const response = await fetch(`${API_BASE_URL}/api/user/me`, {
+      method: "GET",
+      credentials: "include"
+    });
+    const data = await response.json();
+    if (data){
+      console.log(data);
+    }
+  }
+  return (
+
+    <div className="min-h-screen bg-gray-900 text-white p-6">
+
+    <button onClick={ping}>
+      PING
+    </button>
+    <button onClick={me}>
+    ME
+    </button>
+      <h1 className="text-3xl font-bold mb-6">Dashboard</h1>
+      <KanbanBoard />
+    </div>
+  );
+};
+
+export default Dashboard;
