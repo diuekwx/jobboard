b573dbe751e523393db65380c46fcd9c3152643f
Jerison <jerisonw@gmail.com>
2025-09-10 23:04:42 -0700

    hehe

---
+++ backend/models/db_applicationsync.py
@@ -0,0 +1,25 @@
+from backend.db.base_class import Base
+from sqlalchemy import ForeignKey, TIMESTAMP, UUID
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime, timezone
+import uuid
+
+class ApplicationSync(Base):
+    __tablename__ = "application_sync"
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+
+    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), unique=True)
+
+    start_date: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), default=datetime.now(timezone.utc))
+
+    last_synced_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), default=datetime.now(timezone.utc))
+
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), default=datetime.now(timezone.utc))
+    updated_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True),
+        default=datetime.now(timezone.utc),
+        onupdate=datetime.now(timezone.utc)
+    )
+
+    user = relationship("User", back_populates="application_sync")
