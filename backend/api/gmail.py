901f3dd7058a06d73d141ce429730ad4aad4d4a5
Jerison <jerisonw@gmail.com>
2025-09-12 01:40:22 -0700

    updated query for date check

--- backend/api/gmail.py
+++ backend/api/gmail.py
@@ -8,10 +8,29 @@ from backend.models.db_users import User
 from backend.service.oauth_service import refresh_google_token
 import os
 from backend.service.gmail_service import extract_company, extract_company_spacy
+from backend.service.jobs_service import create_job_service
+from backend.service.sync_service import sync, get_last_updated, update_sync_after_fetch
+from datetime import datetime, timezone
+
 router = APIRouter(tags=["gmail"])
 
 @router.get("/fetch-applications")
 def fetch_job_applications(current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+        
+        #check synchronization 
+        # if there is a time inside the db call this fetch first, if not call the other one first ?  
+    last_updated = get_last_updated(db, current_user.id)
+    search_after_timestamp: int
+
+    if last_updated.last_synced_at:
+        if isinstance(last_updated.last_synced_at, datetime):
+            search_after_timestamp = int(last_updated.last_synced_at.timestamp())
+        else:
+            search_after_timestamp = int(last_updated.last_synced_at)
+
+    else:
+        # if they havent updated yet, use the start_day
+        search_after_timestamp = int(last_updated.start_date.timestamp())
 
     integration_token = db.query(IntegrationToken).filter(
         IntegrationToken.user_id == current_user.id,
@@ -33,7 +52,7 @@ def fetch_job_applications(current_user: User = Depends(get_current_user), db: S
     )
     
     service = build("gmail", "v1", credentials=creds)
-    query = 'in:inbox subject:("thank you for applying" OR "application received" OR "application submitted")'
+    query = f'in:inbox subject:("thank you for applying" OR "application received" OR "application submitted") after:{search_after_timestamp}'
 
     results = service.users().messages().list(userId="me", q=query).execute()
     messages = results.get("messages", [])
@@ -41,7 +60,6 @@ def fetch_job_applications(current_user: User = Depends(get_current_user), db: S
         return {"message": "No new job application emails found."}
 
     fetched_applications = []
-
 
     for msg in messages:
         full_msg = service.users().messages().get(userId="me", id=msg["id"]).execute()
@@ -66,5 +84,8 @@ def fetch_job_applications(current_user: User = Depends(get_current_user), db: S
             "date": email_date,
             "status": "sent"
         })
+
+    
+    update_sync_after_fetch(db, current_user.id)
     
     return {"message": "Emails fetched successfully", "applications": fetched_applications} 
